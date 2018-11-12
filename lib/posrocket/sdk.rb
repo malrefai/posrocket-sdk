32a8ee0a87e6a39019be0ed5d166eeebea662bd1
Mohammad Alrefai <malrefai@nyit.edu>
2018-11-12 14:59:03 +0200

    Initial commit

---
+++ lib/posrocket/sdk.rb
@@ -0,0 +1,8 @@
+require "posrocket/sdk/version"
+
+module Posrocket
+  module Sdk
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/posrocket/sdk/version.rb
@@ -0,0 +1,5 @@
+module Posrocket
+  module Sdk
+    VERSION = "0.1.0"
+  end
+end
